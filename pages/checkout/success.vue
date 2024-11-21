e972300e41683f39ab6c7c78340ac41309ff5387
Davide Specchia <d.specchia5@gmail.com>
2024-11-21 17:17:49 +0100

    feat: enhance success page

--- pages/checkout/success.vue
+++ pages/checkout/success.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1 class="text-2xl">Success</h1>
+  <div class="flex h-screen w-full flex-col items-center justify-center">
+    <h1 class="text-3xl">{{ $t("success.title") }}</h1>
+    <div class="text-xl">
+      <i18n-t scope="global" keypath="success.subTitle" as="h2">
+        <span class="italic text-primary-500">{{
+          $t("success.subTitleAccent")
+        }}</span>
+      </i18n-t>
+    </div>
+    <p class="mt-8">{{ $t("success.message") }}</p>
   </div>
 </template>
 
